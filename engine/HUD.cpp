fb561b44366dd59cbfb012c38d48fa1ecbf0f7b9
ScriptedSnark <51358194+ScriptedSnark@users.noreply.github.com>
2024-06-11 19:56:21 +0500

    hw: HUD.cpp

--- engine/HUD.cpp
+++ engine/HUD.cpp
@@ -1,21 +1,36 @@
 #include "quakedef.h"
 #include "gl_screen.h"
+#include "vid.h"
 #include "HUD.h"
 
 void HudSizeUp()
 {
-	if( scr_viewsize.value < 120.0 )
+	float value;
+
+	if (scr_viewsize.value < 120.0f)
 	{
-		float value = scr_viewsize.value + 10.0;
-		Cvar_SetValue( "viewsize", value );
+		value = scr_viewsize.value + 10.0f;
+		Cvar_SetValue("viewsize", value);
 	}
 }
 
 void HudSizeDown()
 {
-	if( scr_viewsize.value > 30.0 )
+	float value;
+
+	if (scr_viewsize.value > 30.0f)
 	{
-		float value = scr_viewsize.value - 10.0;
-		Cvar_SetValue( "viewsize", value );
+		value = scr_viewsize.value - 10.0f;
+		Cvar_SetValue("viewsize", value);
 	}
+}
+
+// UNUSED
+void Sbar_Geiger()
+{
+}
+
+void Sbar_Draw()
+{
+	// CAN'T BE IMPLEMENTED AT THIS MOMENT
 }
